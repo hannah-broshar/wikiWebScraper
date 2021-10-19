4897333cb5856aaee77969d8417547078ddfbfa5
hannah-broshar <hannahbroshar.98@gmail.com>
2021-10-19 13:11:40 -0500

    adding basic wiki web scraper to get Headers

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System;
+
+namespace webapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://en.wikipedia.org/wiki/The_Beatles");
+
+            var HeaderNames = doc.DocumentNode.SelectNodes("//span[@class='toctext']");
+
+            var titles = new List<Row>();
+            foreach (var item in HeaderNames)
+            {
+                titles.Add(new Row { Title = item.InnerText });
+            }
+            using (var writer = new StreamWriter("C:/Users/hanna/OneDrive/Desktop/Code/example.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(titles);
+            }
+        }
+
+        public class Row
+        {
+            public string Title { get; set; }
+        }
+    }
+}
